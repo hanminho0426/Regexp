d209f73dfd43bccabeb220f135afd2b5dc81bc3d
hanminho0426 <l9930426@naver.com>
2021-08-17 18:17:08 +0900

    README.md추가

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(123)
+let str = `
+010-1234-5678
+1234@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps the lazy dog.
+abbccccdddd
+`
+
+//const regexp = new RegExp('the','gi') //g는 소문자 gi 대소문자 구분없이 찾는 플래그
+//const regexp = /the/gi //리터널 방식
+//console.log(str.match(regexp))
+
+const regexp = /fox/gi
+//console.log(regexp.test(str)) //찾는 값이 있는지 true false
+str = str.replace(regexp, 'AAA') //찾아서 변경한 후 반환
+console.log(str)
